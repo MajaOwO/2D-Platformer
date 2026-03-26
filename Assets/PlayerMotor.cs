525945392a01ab94f3e45ab0ad2c3773600a31cb
MajaOwO <dsw63477@student.dsw.edu.pl>
2026-03-26 09:42:59 +0100

    26.03

--- Assets/PlayerMotor.cs
+++ Assets/PlayerMotor.cs
@@ -7,16 +7,45 @@ public class PlayerMotor : MonoBehaviour
     private bool canJump = true;
     private Rigidbody2D rigidbody2D;
     public float speed = 10;
+    public float maxSpeed = 10;
+    public float jumpForce = 10;
+    public float stoppingForce = 5;
+
     public float Jumpforce = 10;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Start()
     {
+        NewMethod();
+    }
+
+    private void NewMethod()
+    {
         rigidbody2D = GetComponent<Rigidbody2D>();
     }
+
     // Update is called once per frame
-    private void Update()
+    private void FixedUpdate()
     {
-        transform.position += new Vector3(direction.x, direction.y, 0) * Time.deltaTime * speed;
+        rigidbody2D.AddForce(new Vector2(direction.x * speed, 0));
+        if (rigidbody2D.linearVelocityX >= maxSpeed)
+        {
+            rigidbody2D.linearVelocityX = maxSpeed;
+        }
+        else if (rigidbody2D.linearVelocityX <= -maxSpeed)
+        {
+            rigidbody2D.linearVelocityX = -maxSpeed;
+        }
+
+        PlayerStopping();
+    }
+
+    private void PlayerStopping()
+    {
+        if (direction.x == 0 && rigidbody2D.linearVelocityX != 0)
+
+        {
+            rigidbody2D.AddForce(new Vector2(-rigidbody2D.linearVelocityX * stoppingForce, 0));
+        }
     }
 
     private void OnMove(InputValue value)
